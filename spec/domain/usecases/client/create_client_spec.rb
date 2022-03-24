45b1baaa04a2bfec29438b4df2142c8581593b30
Aislan Maia <aislan.sousamaia@gmail.com>
2022-03-23 22:08:39 -0300

    Fix tests

--- spec/domain/usecases/client/create_client_spec.rb
+++ spec/domain/usecases/client/create_client_spec.rb
@@ -8,14 +8,41 @@ module UseCases
       it 'should create with success when user is admin' do
         create(:client)
         user = create(:user_admin)
-        params = build(:client).as_json.merge(user: user)
+        params = {
+            name: 'Cliente 1',
+            email: 'cliente1@email.com',
+            phone: 123456789,
+            address: {
+              street: 'Rua 1',
+              number: '123',
+              neighborhood: 'Bairro 1',
+              city: 'Picos',
+              uf: 'PI'
+            },
+          user: user
+        }
+        # params = build(:client).as_json.merge(user: user)
         result = ::UseCases::Client::CreateClient.new(params).call
         expect(result.success?).to be_truthy
       end
       it 'should create with success when user is attendant' do
-        create(:client)
+        client = create(:client)
         user = create(:user_attendant)
-        params = build(:client).as_json.merge(user: user)
+        params = {
+          id: client.id,
+          name: 'Cliente 1',
+          email: 'cliente1@email.com',
+          phone: 123456789,
+          address: {
+            street: 'Rua 1',
+            number: '123',
+            neighborhood: 'Bairro 1',
+            city: 'Picos',
+            uf: 'PI'
+          },
+          user: user
+        }
+        # params = build(:client).as_json.merge(user: user)
         result = ::UseCases::Client::CreateClient.new(params).call
         expect(result.success?).to be_truthy
       end
