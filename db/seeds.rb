7e8f3b2839563c68b6581839a7faff41921caf46
Aislan Maia <aislan.sousamaia@gmail.com>
2022-04-06 22:37:03 -0300

    WIP: Schedules use cases

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 User.create(username: 'admin', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: 'admin@admin.com', password: "123456", role: 1, registry: 111)
 User.create(username: 'attendant1', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: 'atendente1@email.com', password: "123456", role: 2, registry: 222)
-User.create(username: 'technician1', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: 'tecnico1@email.com', password: "123456", role: 3, registry: 333)
-User.create(username: 'technician2', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: 'tecnico2@email.com', password: "123456", role: 3, registry: 444)
+technician1 = User.create(username: 'technician1', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: 'tecnico1@email.com', password: "123456", role: 3, registry: 333)
+technician2 = User.create(username: 'technician2', firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: 'tecnico2@email.com', password: "123456", role: 3, registry: 444)
 
-Client.create(name: 'Carolyn T. Moore', email: 'carolyntmoore@jourrapide.com', phone: '67986453199', address: {
+c1 = Client.create(name: 'Carolyn T. Moore', email: 'carolyntmoore@jourrapide.com', phone: '67986453199', address: {
   street: 'Rua A',
   neighborhood: 'Centro',
   number: '123',
@@ -19,7 +19,7 @@ Client.create(name: 'Carolyn T. Moore', email: 'carolyntmoore@jourrapide.com', p
   uf: 'SP'
 })
 
-Client.create(name: 'Leonardo Carvalho Melo', email: 'carolyntmoore@jourrapide.com', phone: '3149735913', address: {
+c2 = Client.create(name: 'Leonardo Carvalho Melo', email: 'leonardo@transpil.com', phone: '3149735913', address: {
   street: 'José Correa',
   neighborhood: 'Centro',
   number: '1296',
@@ -27,7 +27,7 @@ Client.create(name: 'Leonardo Carvalho Melo', email: 'carolyntmoore@jourrapide.c
   uf: 'MG'
 })
 
-Client.create(name: 'Júlio Souza Martins', email: 'juliosouzamartins@lodash.com', phone: '45986641584', address: {
+c3 = Client.create(name: 'Júlio Souza Martins', email: 'juliosouzamartins@lodash.com', phone: '45986641584', address: {
   street: 'Landulfo Alves',
   neighborhood: 'Centro',
   number: '1721',
@@ -35,10 +35,15 @@ Client.create(name: 'Júlio Souza Martins', email: 'juliosouzamartins@lodash.com
   uf: 'BA'
 })
 
-Client.create(name: 'Isabella Azevedo Fernandes', email: 'isabellaazevedofernandes@rhyta.com', phone: '1678055309', address: {
+c4 = Client.create(name: 'Isabella Azevedo Fernandes', email: 'isabellaazevedofernandes@rhyta.com', phone: '1678055309', address: {
   street: 'Coronel Mariano de Melo',
   neighborhood: 'Centro',
   number: '1571',
   city: 'Ribeirão Preto',
   uf: 'SP'
 })
+
+Schedule.create(client_id: c1.id, user: technician1, scheduling_date: DateTime.new(2022, 3, 30, 8, 0, 0), status: :open)
+Schedule.create(client_id: c2.id, user: technician1, scheduling_date: DateTime.new(2022, 3, 30, 9, 0, 0), status: :open)
+Schedule.create(client_id: c3.id, user: technician2, scheduling_date: DateTime.new(2022, 3, 30, 8, 0, 0), status: :open)
+Schedule.create(client_id: c4.id, user: technician2, scheduling_date: DateTime.new(2022, 3, 30, 9, 0, 0), status: :open)
